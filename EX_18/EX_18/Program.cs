383a4ffa6cd70e0d308886ae0ec868c02546ea63
Pedro Bruno <37937158+Pedrozila@users.noreply.github.com>
2022-04-04 12:16:23 -0300

    Add files via upload

---
+++ EX_18/EX_18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EX_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o PIN: ");
+            int pin = int.Parse(Console.ReadLine());
+
+            while(pin != 2002)
+            {
+                Console.WriteLine("Pin incorreto, tente novamente: ");
+                pin = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Pin correto, acesso liberado!!");
+        }
+    }
+}
