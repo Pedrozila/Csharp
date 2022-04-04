8d2b6ea56f5e096a83ec64783b2f794cf50c0239
Pedro Bruno <37937158+Pedrozila@users.noreply.github.com>
2022-04-04 12:23:23 -0300

    Add files via upload

---
+++ Projetos/Exemplo_For/Exemplo_For/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exemplo_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantas vezes quer repetir? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+
+            }
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
---
+++ Projetos/Exemplo_While/Exemplo_While/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exemplo_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            double x = double.Parse(Console.ReadLine());
+
+            while(x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz);
+
+                Console.WriteLine("digite outro numero: ");
+                x = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("numero invalido");
+        }
+    }
+}
